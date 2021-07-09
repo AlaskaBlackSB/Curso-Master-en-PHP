1c1ddedf88a34fcbffcd087a9b6a6bcba357a154
Agner Alexis <agner1803@hotmail.com>
2021-07-09 12:42:30 -0500

    Agregué el código para acceder a las superglobales

---
+++ introduccion_php/06-superglobales/index.php
@@ -0,0 +1,18 @@
+<?php
+
+//Variables de servidor
+echo "<pre>";
+var_dump( $_SERVER );
+echo "</pre>";
+
+//Variable POST
+
+echo "<pre>";
+var_dump( $_POST );
+echo "</pre>";
+
+//Variable GET
+
+echo "<pre>";
+var_dump( $_GET );
+echo "</pre>";
